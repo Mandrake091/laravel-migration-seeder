381e1e02e7ca62a1e75f98db66dfbf34339be337
Paolo Cannistraro <wasdgamer91@gmail.com>
2022-06-14 17:23:16 +0200

    some adjustment

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,9 +9,14 @@
 
             <div class="col-12 text-center">
                 <h1>Benvenuto!</h1>
-                  <button class="btn btn-primary">
-            <a class="text-white text-decoration-none " href="/hotels"><strong>Tutti gli Hotels</strong></a>
-        </button>
+
+                  
+            <a class="text-white text-decoration-none " href="/hotels">
+                <button class="btn btn-primary">
+                <strong>Tutti gli Hotels</strong>
+             </button>    
+            </a>
+       
             </div>
         </div>
 
